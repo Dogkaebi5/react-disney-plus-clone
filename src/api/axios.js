4cfe776f0982f328251664140cb620fa3453eee4
Dogkaebi5 <st9dog@gmail.com>
2023-02-22 22:36:33 +0900

    api주소 http https로 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "8c7c478de1e8748c7b48fee7c920fe94",
     language: "ko-KR",
